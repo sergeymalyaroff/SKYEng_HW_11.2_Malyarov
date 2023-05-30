52ab7df069105a54f3b403b49ae397b331c0386c
Sergey Malyarov <sergey@malyaroff.com>
2023-05-30 10:54:40 +0300

    Add function takes a string and returns it with the first letter of each word capitalized

--- str_func.py
+++ str_func.py
@@ -1,2 +1,10 @@
 def to_uppercase(input_string):
+    """
+        This function takes a string and returns it with all capital letters.
+    """
     return input_string.upper()
+def capitalize_first_letters(input_string):
+    """
+    This function takes a string and returns it with the first letter of each word capitalized.
+    """
+    return input_string.title()
